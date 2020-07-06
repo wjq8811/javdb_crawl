623779dacfb522522c4eb747e53e28c98bc247f2
wjq8811 <38562196+wjq8811@users.noreply.github.com>
2020-07-06 09:48:26 +0800

    Update main_crawler_one_artor_works.py

--- main_crawler_one_artor_works.py
+++ main_crawler_one_artor_works.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3  
 # -*- coding: utf-8 -*- 
 
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# from selenium import webdriver
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 from lxml import etree
 import os
 import time
@@ -21,22 +22,23 @@ def function(browser,main_url,actor_url,file_path):
 
 def main(main_url,actor_url,file_path):
     # 定义浏览器，不加载图片，跳过认证
-    options = webdriver.ChromeOptions()
-    prefs = {
-        'profile.default_content_setting_values': {
-            'images': 2
-        }
-    }
-    options.add_experimental_option('prefs', prefs)
+    # options = webdriver.ChromeOptions()
+    # prefs = {
+        # 'profile.default_content_setting_values': {
+            # 'images': 2
+        # }
+    # }
+    # options.add_experimental_option('prefs', prefs)
     #设置代理，不需要的可以注释掉
     #options.add_argument('--proxy-server=http://192.168.2.1:1282') 
-    capa = DesiredCapabilities.CHROME
-    capa["pageLoadStrategy"] = "none" #懒加载模式，不等待页面加载完毕
-    browser = webdriver.Chrome(chrome_options=options,desired_capabilities=capa)
+    # capa = DesiredCapabilities.CHROME
+    # capa["pageLoadStrategy"] = "none" #懒加载模式，不等待页面加载完毕
+    # browser = webdriver.Chrome(chrome_options=options,desired_capabilities=capa)
     #跳过验证
-    my_selenium.i_am_robot(browser, main_url)
+    # my_selenium.i_am_robot(browser, main_url)
 
     function(browser,main_url,actor_url,file_path)
+
 
 
 
