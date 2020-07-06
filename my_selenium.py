55e53e73003c4d05185c5cd631d925d4b80fc4bc
wjq8811 <38562196+wjq8811@users.noreply.github.com>
2020-07-06 17:21:16 +0800

    Update my_selenium.py

--- my_selenium.py
+++ my_selenium.py
@@ -23,39 +23,38 @@ def steal_library_header(url):
 
 def get_html_by_requests(header,url, xpath_):
     try:
-        html = requests.get(url,headers=header)
+        html = requests.get(url,headers=header, timeout=15)
     except Exception as e:
-        print('无法按时打开网页，五秒后重试。')
+        print('无法正确打开网页，五秒后重试。')
         time.sleep(5)
-        html, html_xpath = get_html_by_requests(header,url, xpath_)
-
+        html, html_text, html_xpath = get_html_by_requests(header,url, xpath_)
     # print(url)
     # print(html)
     if html.status_code == 200:
-        html = html.text
-        if '暫無內容' in html:
+        html_text = html.text
+        if '暫無內容' in html_text:
             time.sleep(1)
-            html_xpath = etree.HTML(html)
-            return html, html_xpath
-        if 'javdb' not in html:
+            html_xpath = etree.HTML(html_text)
+            return html_text, html_xpath
+        if 'javdb' not in html_text:
             print('html中找不到javdb，五秒后重试。')
             time.sleep(5)
-            html, html_xpath = get_html_by_requests(header,url, xpath_)
-        if html == '':
+            html, html_text, html_xpath = get_html_by_requests(header,url, xpath_)
+        if html_text == '':
             print('html为空，五秒后重试。')
             time.sleep(5)
-            html, html_xpath = get_html_by_requests(header,url, xpath_)
-        html_xpath = etree.HTML(html)
+            html, html_text, html_xpath = get_html_by_requests(header,url, xpath_)
+        html_xpath = etree.HTML(html_text)
         if html_xpath.xpath(xpath_) is None:
             print('html_xpath为空，五秒后重试。')
             time.sleep(5)
-            html, html_xpath = get_html_by_requests(header,url, xpath_)
-        time.sleep(2)
+            html, html_text, html_xpath = get_html_by_requests(header,url, xpath_)
+        time.sleep(0.5)#成功后间隔时间
     else:
-        print('无法按时打开网页，五秒后重试。')
+        print('响应错误，五秒后重试。')
         time.sleep(5)
-        html, html_xpath = get_html_by_requests(header,url, xpath_)
-    return html, html_xpath
+        html_text, html_xpath = get_html_by_requests(header,url, xpath_)
+    return html, html_text, html_xpath
 
 if __name__ == '__main__':
     url = 'https://javdb4.com'
