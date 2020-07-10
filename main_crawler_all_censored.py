f65da33605630bdfc42da35a176289aa514279a7
wjq8811 <38562196+wjq8811@users.noreply.github.com>
2020-07-10 15:54:51 +0800

    Add files via upload

---
+++ main_crawler_all_censored.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3  
+# -*- coding: utf-8 -*- 
+import file_io,my_selenium,crawler_function,main_crawler_one_artor_works
+import requests
+import os
+
+def main(main_url,file_path):
+    header = my_selenium.steal_library_header(url=main_url)
+    # try:
+    #抓取所有演员地址并保存
+    print('文件保存在：'+file_path)
+    actors_list_path = file_path + '\\' + 'actors_list.txt'
+    if os.path.exists(actors_list_path):
+        print('actors_list已存在，如需更新请删除后重新开始。')
+        actors_list = file_io.read_all_lines(actors_list_path)
+    else:
+        actors_list = crawler_function.crawler_all_actros(header, main_url)
+        file_io.write_all_lines(actors_list_path, actors_list)
+
+    for tmp in actors_list:
+        try:
+            actor_name,actor_url = tmp.split('|')
+            print(actor_name,actor_url)
+            main_crawler_one_artor_works.function(header,main_url,actor_url,file_path)
+        except Exception as e:
+            continue
+
+
+
+    # except Exception as e:
+    #     main(main_url,file_path)
+    # print('文件保存在：'+file_path)
+
+
+if __name__ == '__main__':
+    main_url = 'https://javdb4.com'
+    file_path = r'Y:\javdb\censored'
+    main(main_url,file_path)
---
+++ main_crawler_series.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3  
+# -*- coding: utf-8 -*- 
+import requests
+from lxml import etree
+import os
+import time
+import json
+import file_io,my_selenium,crawler_function
+
+def main(main_url, file_path):
+    header = my_selenium.steal_library_header(url=main_url)
+    type_video = ['LUXU','ARA','SIRO','GANA','MIUM']
+    for x in type_video:
+        serise_path = file_path + '\\' + x
+        serise_url = 'https://javdb4.com/video_codes/' + x
+        work_list_path = file_path + '\\' + 'work_list.txt'
+        if os.path.exists(work_list_path):
+            print('work_list.txt已存在，如需更新，请删除后重新开始。')
+            #读取url，爬取页面
+            work_list = file_io.read_all_lines(work_list_path)
+        else:
+            #爬取页面，保存url
+            work_list = crawler_function.crawler_serise_works(header,serise_url,main_url)
+            file_io.write_all_lines(work_list_path, work_list)
+
+        crawler_function.crawler_top_work(header, main_url,work_list,file_path)
+        print('文件保存在：'+top_path)
+        print('-'*20)
+
+if __name__ == '__main__':
+    main_url = 'https://javdb4.com'
+    file_path = r'C:\javdb\serise'
+    main(main_url, file_path)
+
