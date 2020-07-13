33f992b61000c916794895384428d1e946cf12c0
wjq8811 <38562196+wjq8811@users.noreply.github.com>
2020-07-13 10:04:48 +0800

    Update main_crawler_series.py

--- main_crawler_series.py
+++ main_crawler_series.py
@@ -12,19 +12,22 @@ def main(main_url, file_path):
     type_video = ['LUXU','ARA','SIRO','GANA','MIUM']
     for x in type_video:
         serise_path = file_path + '\\' + x
+        file_io.create_dir_if_not_exist(serise_path)
         serise_url = 'https://javdb4.com/video_codes/' + x
-        work_list_path = file_path + '\\' + 'work_list.txt'
+        work_list_path = serise_path + '\\' + 'work_list.txt'
         if os.path.exists(work_list_path):
             print('work_list.txt已存在，如需更新，请删除后重新开始。')
             #读取url，爬取页面
             work_list = file_io.read_all_lines(work_list_path)
         else:
+            pass
+            print('work_list.txt不存在')
             #爬取页面，保存url
             work_list = crawler_function.crawler_serise_works(header,serise_url,main_url)
             file_io.write_all_lines(work_list_path, work_list)
 
-        crawler_function.crawler_top_work(header, main_url,work_list,file_path)
-        print('文件保存在：'+top_path)
+        crawler_function.crawler_top_work(header, main_url,work_list,serise_path)
+        print('文件保存在：'+serise_path)
         print('-'*20)
 
 if __name__ == '__main__':
