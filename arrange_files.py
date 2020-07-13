e7b0628248f29a361b704d4011ea8a3f3b36977d
wjq8811 <38562196+wjq8811@users.noreply.github.com>
2020-07-13 12:48:30 +0800

    Update arrange_files.py

--- arrange_files.py
+++ arrange_files.py
@@ -21,35 +21,54 @@ def sql_query():
     connect.close()
     return tmp_list
 
-def find_censored_images(censored_images_path):
-    for file_name in os.listdir(censored_images_path):
-        file_path = os.path.join(censored_images_path,file_name)
-        if '.jpg' in file_path:
-            print(file_name)
-            print(file_path)
-            # for images_path in os.listdir(file_path):
-            #     images_path = os.path.join(file_path,images_path)
-            #     
-            #         print(json_path)
-            #         html_path = json_path.replace('json','html')
-            #         print(html_path)
-            #         new_file_path = json_path.replace('.json','')
-            #         print(new_file_path)
-            #         fanhao = new_file_path.split('\\')[-1]
-            #         print(fanhao)
-            #         print('-'*20)
-            #         if os.path.exists(new_file_path):
-            #             pass
-            #         else:
-            #             os.mkdir(new_file_path)
-            #         shutil.move(html_path, new_file_path)
-            #         shutil.move(json_path, new_file_path)
-            #         new_json_path = os.path.join(new_file_path,fanhao+'.json')
-
+def arrange_censored_images(censored_path,censored_images_path):
+    tmp_list = sql_query()
+    print('sql_query_ok')
+    #先找html路径
+    num = 1 
+    for root,dirs,files in os.walk(censored_path):
+        for html_name in files:
+            html_path = os.path.join(root,html_name)
+            if '.html' in html_path:
+                print(num,'_'*20)
+                print(html_name.encode('gbk', 'ignore').decode('gbk'))
+                print(html_path.encode('gbk', 'ignore').decode('gbk'))
+                html_fanhao = html_name.replace('.html','')
+                html_fanart_path = root + '\\' + html_fanhao + '-fanart.jpg'
+                if os.path.exists(html_fanart_path):
+                    print('fanart已存在')
+                    continue
+                else: 
+                    for tmp in tmp_list:
+                        sql_fanhao,actors,video_cover,preview_video,preview_pictures = tmp
+                        sql_img_name = ''
+                        if sql_fanhao == html_fanhao:
+                            sql_img_name = video_cover.split('/')[-1]
+                            break
+                    if sql_img_name == '':
+                        print('数据库中找不到html_fanhao')
+                        continue
+                    else:
+                        for img_name in os.listdir(censored_images_path):
+                            img_path = os.path.join(censored_images_path,img_name)
+                            if sql_img_name == img_name:
+                                old_img_path = img_path
+                                print(old_img_path)
+                                #移动
+                                shutil.copy(old_img_path,root)
+                                print('移动成功')
+                                #重命名
+                                old_name = root + '\\' + img_name
+                                new_name = root + '\\' + sql_fanhao + '-fanart.jpg'
+                                os.rename(old_name,new_name)
+                                print('重命名成功')
+                                num +=1
+                                break
 
 if __name__ == '__main__':
-    censored_images_path = r'Z:\censored_images'
-    find_censored_images(censored_images_path)
+    censored_images_path = r'E:\javdb\censored\censored_poster'
+    censored_path = r'Z:\censored'
+    arrange_censored_images(censored_path,censored_images_path)
     # tmp_list = sql_query()
     # for x in tmp_list:
     #     print(x[0])
